ab421e3b1f328d3c1965ac0a1c1588f62a082afe
Edward Dong <edwarddong86@gmail.com>
2015-06-26 14:24:31 -0700

    #1 working on search query

--- server.js
+++ server.js
@@ -5,12 +5,40 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var port = process.env.PORT || 8080;
+var yelp = require('node-yelp').createClient({
+  consumer_key: "O1QsJq-LZt8AudUxN5bNEA",
+  consumer_secret: "nCNX2V_-fOXFNGn9Xqtu3__SVc8",
+  token: "Eoqm1zjVHUoY-bvZFBhNPT12Wch7MgOT",
+  token_secret: "jsdzQAYdwW5cWDDw-NVpfzK3_4M"
+});
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
+  res.sendFile(path.join(__dirname + '/public/app/views/pages/index.html'));
+});
+
+app.use('/', function(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+app.get('/images/:location', function(req, res) {
+
+  //var searchParameters = {};
+  //searchParameters.terms = "food";
+  //searchParameters.location = req.params.location;
+  //
+  //yelp.search(searchParameters).then(function (data) {
+  //  var images = [];
+  //  for (var i = 0; i < data.businesses.length; i++) {
+  //    images.push(data.businesses[i].images_url);
+  //  }
+  //  return res.send(images);
+  //});
+
+
 });
 
 app.listen(port);
-console.log('Port:' + port);
